5856660c4b81c97cf995dc4f5f887596b5be34a5
AshwiniKumar <36327547+AshwiniatGit@users.noreply.github.com>
2019-04-01 17:12:57 +0530

    Add files via upload

---
+++ JsonParser/src/main/java/com/cts/JsonParser/App.java
@@ -0,0 +1,41 @@
+package com.cts.JsonParser;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class App {
+
+  private static String readAll(Reader rd) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    int cp;
+    while ((cp = rd.read()) != -1) {
+      sb.append((char) cp);
+    }
+    return sb.toString();
+  }
+
+  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+    InputStream is = new URL(url).openStream();
+    try {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONObject json = new JSONObject(jsonText);
+      return json;
+    } finally {
+      is.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException, JSONException {
+    JSONObject json = readJsonFromUrl("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=18.5971,73.7188&radius=1500&type=regions&key=AIzaSyDIT6TcATl_SOeXkPJes-8-WgmVe2zljFE");
+    System.out.println(json.toString());
+    System.out.println(json.get("vicinity"));
+  }
+}
